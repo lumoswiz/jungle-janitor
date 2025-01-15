e84d427dc5adf78e92226e0710e03fdfc22a9412
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-01-15 14:47:51 +0100

    chore: additional project setup and configuration (#1)
    Changes:
    - Added setuptools dependency
    - Added silverback-sessions to gitignore

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
+import os
+
+from ape import Contract
 from silverback import SilverbackBot
 
 # Initialize bot
 bot = SilverbackBot()
+
+# Pool Addresses Provider contract
+POOL_ADDRESSES_PROVIDER = Contract(os.environ["POOL_ADDRESSES_PROVIDER"])
+
+# Pool contract
+POOL = Contract(POOL_ADDRESSES_PROVIDER.getPool())
